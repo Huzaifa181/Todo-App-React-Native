57d5c0d6d2a4531be5a5288614c70c7b6191c125
Huzaifa181 <huzaifa.ahmed181@gmail.com>
2021-07-20 18:13:39 +0500

    Create todo list item

--- TodoList.js
+++ TodoList.js
@@ -7,7 +7,7 @@ import {
   useColorScheme,
   View,
 } from 'react-native';
-
+// import {Icon} from 'react-native-elements';
 import {
   NativeBaseProvider,
   Container,
@@ -16,21 +16,35 @@ import {
   Box,
   Button,
   SimpleGrid,
+  IconButton,
+  Checkbox,
   VStack,
   HStack,
+  Icon,
   List,
 } from 'native-base';
 import {} from 'react-native/Libraries/NewAppScreen';
 
 const Todo = ({todoList}) => {
   const isDarkMode = useColorScheme() === 'dark';
-  
+
   return (
     <List spacing={2} my={2}>
-      {todoList.map((ele)=>{
-        return(
-          <List.Item key={ele.id}>{ele.title}</List.Item>
-        )   
+      {todoList.map(ele => {
+        return (
+          <List.Item key={ele.id}>
+            <Text style={{flex: 1}}>{ele.id}</Text>
+            <View style={{flex: 1}}>
+              <Checkbox
+                value="test"
+                accessibilityLabel="This is a dummy checkbox"
+                defaultIsChecked
+              />
+            </View>
+            <Text style={{flex: 6}}>{ele.title}</Text>
+            <Icon name="trash" style={{flex: 1}} />
+          </List.Item>
+        );
       })}
     </List>
   );
